f62cc0bba02b15cb1c1222587fcda6600981a6fb
Hector Lazcano <h-lazcano@tti.tamu.edu>
2022-06-07 08:41:07 -0500

    enhanced

--- client.py
+++ client.py
@@ -1,13 +1,43 @@
 import socket
 
+HEADER = 64 #THE HEADER TELLS NUMBER OF BYTES THAT WE WILL REQUIRE FOR THE NEXT MESSAGE
+PORT = 1120
+SERVER = socket.gethostbyname(socket.gethostname())
+ADDR = (SERVER, PORT)
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!DISCONNECT"
+
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(ADDR)
+
+def send(msg):
+    message = msg.encode(FORMAT)
+    msg_length = len(message)
+    send_length = str(msg_length).encode(FORMAT)
+    send_length += b' ' * (HEADER - len(send_length))
+    client.send(send_length)
+    client.send(message)
+    print(client.recv(2048).decode(FORMAT))
+
+send("HELLO WORLD!")
+input()
+send("This is another test")
+input()
+send("it doesn't matter, the length it will be handled")
+
+send(DISCONNECT_MESSAGE)
+'''
+#Old Code
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((socket.gethostname(), 1120))
+s.connect(("192.168.1.8", 1120))
 
 full_msg = ''
 while True:
-    msg = s.recv(8)
+    msg = s.recv(16)
     if len(msg) <=0:
         break
     full_msg += msg.decode("utf-8")
 
     print(full_msg)
+    '''
--- server.py
+++ server.py
@@ -1,8 +1,53 @@
 import socket
 
+import threading
+
+HEADER = 64 #THE HEADER TELLS NUMBER OF BYTES THAT WE WILL REQUIRE FOR THE NEXT MESSAGE
+PORT = 1120
+SERVER = socket.gethostbyname(socket.gethostname())
+ADDR = (SERVER, PORT)
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!DISCONNECT"
+
+serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+serverSocket.bind(ADDR)
+
+def handle_client(conn, addr):
+    print(f"[NEW CONNECTION] {addr} connected.")
+
+    connected =True
+    while connected:
+
+        msg_length = conn.recv(HEADER).decode(FORMAT)
+        if msg_length:
+            msg_length = int(msg_length)
+            #How many bytes will we receive
+            #Will wait until it receives the msg
+            msg = conn.recv(msg_length).decode(FORMAT)
+            if msg == DISCONNECT_MESSAGE:
+                connected = False
+            print(f"[{addr}]:: {msg}")
+            conn.send("Received message!".encode(FORMAT))
+    conn.close()
+
+#New connections
+def start():
+    serverSocket.listen()
+    print(f"[LISTENING] Server is listening on {ADDR}")
+    while True:
+        conn, addr = serverSocket.accept()
+        thread = threading.Thread(target=handle_client, args=(conn, addr))
+        print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1 }")
+        thread.start()
+
+print(f"Starting server...")
+start()
+
+'''
+#Old Code
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((socket.gethostname(), 1120))
-s.listen(5)
+s.bind(("192.168.1.8", 1120))
+s.listen(16)
 
 while True:
     clientsocket, address = s.accept()
@@ -10,3 +55,4 @@ while True:
     print(f"{address}")
     clientsocket.send(bytes("Successfully connected", "utf-8"))
     clientsocket.close()
+    '''
